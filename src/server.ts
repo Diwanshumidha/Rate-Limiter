75e5d774e5d2d89e0cad04da2b187fcd520fb980
Diwanshu <saidiwanshu1880@gmail.com>
2024-02-18 21:08:33 +0530

    Made Basic Express App

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("hello World");
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("*", function (req, res) {
+  res.status(404).json({ ERROR: "URL NOT FOUND" });
+});
+
+app.listen(port, function () {
+  console.log(`Serve at http://localhost:${port}`);
+});
